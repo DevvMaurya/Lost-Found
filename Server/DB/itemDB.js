85ae0c69c28b02f991c46bc3285d6178528dc8a0
DevvMaurya <mauryadevv@gmail.com>
2024-01-30 15:38:59 +0530

    private info

--- Server/DB/itemDB.js
+++ Server/DB/itemDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/lostFoundDB');
+mongoose.connect('mongodb+srv://user:user@cluster0.339bpnm.mongodb.net/');
 
 const itemSchema = new mongoose.Schema({
   name: {
